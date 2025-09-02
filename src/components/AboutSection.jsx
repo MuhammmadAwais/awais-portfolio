f9a6919b772848487e10b9cb4bb86451cc2c8866
Awais Latif <97788862+MuhammmadAwais@users.noreply.github.com>
2025-09-02 20:26:47 +0500

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -38,7 +38,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/My-CV.pdf"
+                href="/My-Resume.pdf"
                 download="/My-Resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
